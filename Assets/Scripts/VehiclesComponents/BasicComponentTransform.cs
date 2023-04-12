b9622ff9baffb816c9d78119f2e2c24862926190
yopaper <71711655+yopaper@users.noreply.github.com>
2023-04-12 23:01:20 +0800

    2023-04-12-Update-2

--- Assets/Scripts/VehiclesComponents/BasicComponentTransform.cs
+++ Assets/Scripts/VehiclesComponents/BasicComponentTransform.cs
@@ -7,6 +7,11 @@ namespace WMD.VehicelsComponents
 {
     public abstract class BasicComponentTransform
     {
+        // Static
+
+        //===================================================================
+        // Not Static
+
         // 偶數寬度
         public bool EvenWidth => BlockSize.x % 2 == 0;
         // 偶數長度
@@ -28,7 +33,17 @@ namespace WMD.VehicelsComponents
         //-----------------------------------------------------------------
         protected void LoadOffsetSource(){
             void CheckOffset(){
-
+                void CheckOffsetArray( Vector2Int[] array ){
+                    string exceptionMessage = "長或寬為偶數時, 位移量不可為零!";
+                    for(int i=0; i<array.Length; i++){
+                        if( (EvenWidth && array[i].x==0)||(EvenLength && array[i].y==0) )
+                            throw new System.Exception(exceptionMessage);
+                    }
+                }//...................................................
+                if( !EvenWidth && !EvenLength )return;
+                CheckOffsetArray( RootPositionsOffset );
+                CheckOffsetArray( BuildablePositionsOffset );
+                CheckOffsetArray( BlockPositionsOffset );
             }//......................................................
             BlockPositionsOffset = BlockPositionOffsetSource;
             RootPositionsOffset = RootPositionsOffsetSource;
@@ -39,6 +54,20 @@ namespace WMD.VehicelsComponents
         {
             
         }//-----------------------------------------------------------------
-        
+        protected Vector2Int[] OffsetsToIndexPositions( Vector2Int[] offsets ){
+            List< Vector2Int > indexPositions = new List<Vector2Int>();
+            for(int i=0; i<offsets.Length; i++){
+                Vector2 currentOffset = offsets[i];
+                Vector2 offsetDelta = new Vector2(
+                    ( EvenWidth )? -currentOffset.x /Mathf.Abs(currentOffset.x) /2f:0,
+                    ( EvenLength )? -currentOffset.y /Mathf.Abs(currentOffset.y) /2f:0
+                );
+                indexPositions.Add( new Vector2Int(
+                    Mathf.RoundToInt(CenterPosition.x + currentOffset.x + offsetDelta.x),
+                    Mathf.RoundToInt(CenterPosition.y + currentOffset.y + offsetDelta.y)
+                ) );
+            }
+            return indexPositions.ToArray();
+        }//-----------------------------------------------------------------
     }//=================================================================================
 }
