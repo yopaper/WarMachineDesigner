5954ff60c054573321e5a625891757adbee83150
yopaper <71711655+yopaper@users.noreply.github.com>
2023-04-23 22:20:38 +0800

    2023-04-23

--- Assets/Scripts/VehiclesComponents/VehiclesComponent.cs
+++ Assets/Scripts/VehiclesComponents/VehiclesComponent.cs
@@ -6,10 +6,21 @@ namespace WMD.VehicelsComponents
 {
     public abstract class VehicleComponent : MonoBehaviour
     {
+        public enum ComponentScale{ Small, Medium, Large }
         public VehicleComponentTransform ComponentTransform {get; protected set;}
+        public VehicleComponentData ComponentData{get; protected set;}
         public abstract bool BaseComponent {get;}
+        public abstract ComponentScale Scale {get;}
         //--------------------------------------------------------------
         protected abstract VehicleComponentTransform TransformSource{get;}
-        
+        protected abstract VehicleComponentData DataSource{get;}
+        //-------------------------------------------------------------
+        protected void InitialVariable(){
+            ComponentTransform = TransformSource;
+            ComponentData = DataSource;
+        }//-------------------------------------------------------------
+        protected void Awake(){
+            InitialVariable();
+        }//-------------------------------------------------------------
     }//=================================================================
 }
